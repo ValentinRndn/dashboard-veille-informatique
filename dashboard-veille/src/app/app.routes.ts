a10323bc62682ea42b011d126f68085dac1379d8
Valentin <valentinrenaudin2206@gmail.com>
2024-12-06 10:29:44 +0100

    add initial components and routing setup for the dashboard

---
+++ dashboard-veille/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./views/home/home.component').then(c => c.HomeComponent) 
+    },
+  {
+    path: '**',
+    redirectTo: 'home' 
+  }
+];
---
+++ dashboard-veille/src/app/views/niche/niche.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-niche',
+  standalone: true,
+  imports: [],
+  templateUrl: './niche.component.html',
+  styleUrl: './niche.component.css'
+})
+export class NicheComponent {
+
+}
